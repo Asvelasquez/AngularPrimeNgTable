47ca8f08ddc53de9314b6409f664f3218d6b7ac9
3RR0R404 <32886956+Asvelasquez@users.noreply.github.com>
2021-09-12 23:54:04 -0500

    prime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng-lts/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
